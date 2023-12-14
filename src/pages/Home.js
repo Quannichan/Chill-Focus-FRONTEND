8e0bff335f14570a1dee33aea99e0dc0cf947cbb
Trần Minh Quân <tranminhquan130804@gmail.com>
2023-12-14 18:21:03 +0700

    Refactor: delete all

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Helmet} from "react-helmet"
+
+function Home(){
+    return(
+        <Helmet>
+            <title>Chill&Focus | Home</title>
+        </Helmet>
+    );
+}
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Helmet} from "react-helmet"
+
+function Login() {
+        return(
+            <div className="wrap_login">
+                <Helmet>
+                    <title>Chill&Focus | Login</title>
+                </Helmet>
+                <div className="login_ctn">
+                    <form >
+                
+                    </form>
+                </div>
+            </div>
+            
+        );
+}
+export default Login;
